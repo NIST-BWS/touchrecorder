0644a6f7b61ed94216ff067ef3f9282559d950b7
Greg Fiumara <gregory.fiumara@nist.gov>
2013-03-01 12:05:57 -0500

    Demo of shape insertion.

--- Touch Recorder/BWSTRViewController.h
+++ Touch Recorder/BWSTRViewController.h
@@ -8,8 +8,45 @@
  * about its quality, reliability, or any other characteristic.
  */
 
+@class BWSTRShape;
+
 #import <UIKit/UIKit.h>
 
 @interface BWSTRViewController : UIViewController
+
+@property (nonatomic, assign) NSUInteger rows;
+@property (nonatomic, assign) NSUInteger columns;
+@property (nonatomic, assign) NSUInteger shapeWidth;
+@property (nonatomic, assign) NSUInteger shapeHeight;
+@property (nonatomic, strong) UIColor *shapeForegroundColor;
+@property (nonatomic, strong) UIColor *shapeBackgroundColor;
+
+
+#pragma mark - Shape Insertion
+
+/**
+ * @brief
+ * Draw shape in a quadrant.
+ *
+ * @param shape
+ *	Shape to draw.
+ * @param quadrantNumber
+ *	1-based quadrant number, row-major order.
+ */
+- (void)insertShape:(BWSTRShape *)shape inQuadrant:(NSUInteger)quadrantNumber;
+
+/**
+ * @brief
+ * Draw shape in a quadrant.
+ * 
+ * @param shape
+ *	Shape to draw.
+ * @param row
+ *	1-based row in which shape is drawn.
+ * @param column
+ *	1-based column in which shape is drawn.
+ */
+- (void)insertShape:(BWSTRShape *)shape inRow:(NSUInteger)row column:(NSUInteger)column;
+
 
 @end
--- Touch Recorder/BWSTRViewController.m
+++ Touch Recorder/BWSTRViewController.m
@@ -8,9 +8,16 @@
  * about its quality, reliability, or any other characteristic.
  */
 
+#import "BWSTRCircle.h"
+#import "BWSTRSquare.h"
+
+#import "NSMutableArray+BWSTRUtilities.h"
+#import "UIView+Quadrants.h"
 #import "BWSTRViewController.h"
 
 @interface BWSTRViewController ()
+
+@property (nonatomic, strong) NSArray *quadrants;
 
 @end
 
@@ -18,14 +25,43 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[super viewDidLoad];
+	
+	self.rows = 4;
+	self.columns = 3;
+	self.shapeBackgroundColor = [UIColor whiteColor];
+	self.shapeForegroundColor = [UIColor greenColor];
+	self.shapeHeight = 50;
+	self.shapeWidth = 50;
+	
+	for (NSUInteger row = 1; row <= self.rows; row++)
+		for (NSUInteger column = 1; column <= self.columns; column++)
+			[self insertShape:[[BWSTRSquare alloc] init] inRow:row column:column];
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - Shape Insertion
+
+- (void)insertShape:(BWSTRShape *)shape inRow:(NSUInteger)row column:(NSUInteger)column
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	NSAssert(row <= self.rows, @"Invalid row (%u vs %u)", row, self.rows);
+	NSAssert(column <= self.columns, @"Invalid column (%u vs %u)", column, self.columns);
+
+	[self insertShape:shape inQuadrant:(((row - 1) * self.columns) + (column - 1) + 1)];
+}
+
+- (void)insertShape:(BWSTRShape *)shape inQuadrant:(NSUInteger)quadrantNumber
+{
+	NSAssert(quadrantNumber <= (self.rows * self.columns), @"Invalid quadrant (%u vs %u)", quadrantNumber, (self.rows * self.columns) - 1);
+	if (self.quadrants == nil)
+		self.quadrants = [self.view quadrantsWithNumberOfRows:self.rows columns:self.columns];
+	
+	[shape setBackgroundColor:self.shapeBackgroundColor];
+	[shape setForegroundColor:self.shapeForegroundColor];
+	
+	CGRect quadrant = [[self.quadrants objectAtIndex:quadrantNumber - 1] CGRectValue];
+	[shape setFrame:CGRectMake(quadrant.origin.x, quadrant.origin.y, self.shapeWidth, self.shapeHeight)];
+	[shape setCenter:CGPointMake(CGRectGetMidX(quadrant), CGRectGetMidY(quadrant))];
+	[self.view addSubview:shape];
 }
 
 @end
