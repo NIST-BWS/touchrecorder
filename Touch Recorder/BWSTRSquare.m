66367a401670e2482c8c3365c05a477acee6d951
Greg Fiumara <gregory.fiumara@nist.gov>
2013-03-01 10:15:32 -0500

    Add a square shape.

---
+++ Touch Recorder/BWSTRSquare.m
@@ -0,0 +1,41 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology (NIST) by employees of the Federal Government in the course
+ * of their official duties. Pursuant to title 17 Section 105 of the
+ * United States Code, this software is not subject to copyright protection
+ * and is in the public domain. NIST assumes no responsibility whatsoever for
+ * its use by other parties, and makes no guarantees, expressed or implied,
+ * about its quality, reliability, or any other characteristic.
+ */
+
+#import "UIColor+BWSTRUtilities.h"
+
+
+#import "BWSTRSquare.h"
+
+@implementation BWSTRSquare
+
+- (void)drawRect:(CGRect)rect
+{
+	[super drawRect:rect];
+	
+	CGFloat red, green, blue, alpha;
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	CGContextSetShouldAntialias(context, true);
+
+	if ([self.foregroundColor getRed:&red green:&green blue:&blue alpha:&alpha]) {
+		CGContextSetRGBFillColor(context, red, green, blue, alpha);
+		CGContextSetRGBStrokeColor(context, red, green, blue, alpha);
+	} else {
+		CGContextSetRGBFillColor(context, 1, 1, 1, 1);
+		CGContextSetRGBStrokeColor(context, 1, 1, 1, 1);
+	}
+	CGContextFillRect(context, rect);
+	
+	CGContextDrawPath(context, kCGPathFillStroke);
+	
+	/* Save this context to an image for location testing */
+	self.backingImage = [UIImage imageWithCGImage:CGBitmapContextCreateImage(context)];
+}
+
+@end
